174ac9a2a115e45c5168cde8c154f5c932620721
neranga6 <neranga65@gmail.com>
2023-06-15 11:03:01 -0500

    my 1st commit

---
+++ src/main/java/com/webapp/neo/webapp/neo/ContactController.java
@@ -0,0 +1,42 @@
+package com.webapp.neo.webapp.neo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ContactController {
+
+    private final EmailService emailService;
+
+    @Autowired
+    public ContactController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/contact")
+    public String showContactForm(Model model) {
+        model.addAttribute("contactForm", new ContactForm());
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public RedirectView submitContactForm(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("message") String message) {
+
+        String to = "neranga65@gmail.com"; // Replace with the recipient's email address
+        String subject = "New Contact Message";
+        String content = "Name: " + name + "\n"
+                + "Email: " + email + "\n"
+                + "Message: " + message;
+        emailService.sendEmail(to, subject, content);
+
+        return new RedirectView("/contact?success");
+    }
+}
---
+++ src/main/java/com/webapp/neo/webapp/neo/ContactForm.java
@@ -0,0 +1,13 @@
+package com.webapp.neo.webapp.neo;
+
+import lombok.Data;
+
+@Data
+public class ContactForm {
+
+    private String name;
+    private String email;
+    private String message;
+
+    // Add getters and setters
+}
